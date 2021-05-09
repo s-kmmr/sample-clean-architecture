9a313883aeaddb7ddbd51de7f66da848ef3260e1
kamimura <s.kmmr1074@gmail.com>
2021-05-09 19:11:23 +0900

    responseのフィールドをpublicに変更

--- interfaces/controllers/member_controller.go
+++ interfaces/controllers/member_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 
+	"github.com/s-kmmr/sample-clean-architecture/interfaces/controllers/response"
 	"github.com/s-kmmr/sample-clean-architecture/usecases"
 )
 
@@ -20,11 +21,15 @@ func NewMemberController(_u usecases.MemberUseCase) MemberController {
 	return &memberController{u: _u}
 }
 
+type Katsuo struct {
+	Name string
+}
+
 func (m *memberController) MemberList(ctx context.Context, ctrlctx Context) {
 	list, err := m.u.Members(ctx)
 	if err != nil {
 		ctrlctx.JSON(500, err)
 		return
 	}
-	ctrlctx.JSON(200, list)
+	ctrlctx.JSON(200, response.NewMemberResponses(list))
 }
--- interfaces/controllers/response/member_response.go
+++ interfaces/controllers/response/member_response.go
@@ -1,6 +1,22 @@
 package response
 
+import "github.com/s-kmmr/sample-clean-architecture/domain/model"
+
 type MemberResponse struct {
 	LastName  string
 	FirstName string
+}
+
+func NewMemberResponses(m []model.Member) []MemberResponse {
+	if len(m) < 1 {
+		return nil
+	}
+	res := make([]MemberResponse, len(m))
+	for i, v := range m {
+		res[i] = MemberResponse{
+			LastName:  v.LastName(),
+			FirstName: v.FirstName(),
+		}
+	}
+	return res
 }
